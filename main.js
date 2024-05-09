80f871911ec9f35a7e48b93109d1b9a43d2f1584
IrvingOgaz <128095023+IrvingOgaz@users.noreply.github.com>
2024-05-09 09:09:36 -0600

    version 1.0.3

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 var express = require('express');
-var app = express();
+var app=express();
+var server=require('http').Server(app);
+var io = require ('socket.io')(server);
 
-var server = require('http').Server(app);
+/Usamo un middleware para usar elementos estaticos en la seccion publica de la aplicación/
+app.use(express.static('public'));
 
-var io = require('socket.io')(server);
+app.get('/', function(req,res){
+    res.status(200).send("Hola mundo");
+});
 
-app.get('/',function(req, res){
-    res.status(200).send("Hola Mundo")
+/*De esta forma activamos socket para que este escuchando mandamos un mensaje de control por consola para saber que pasa
+y tenemos que hacer que el mensaje venga del navegador web mediante html y JS */ 
+io.on('connection', function(socket){
+    console.log('Alguien se ha conectado con socket')
 });
 
 server.listen(3002, function(){
-    console.log("el servidor esat corriendo en http://localHost:3002");
+    console.log("El servidor esta corriendo en http://localhost:3002");
 });
