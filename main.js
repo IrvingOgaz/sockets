8be750d0f491b8d910547e51d57765890998ce83
IrvingOgaz <128095023+IrvingOgaz@users.noreply.github.com>
2024-05-13 09:58:29 -0600

    aplicacion beta

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app=express();
 var server=require('http').Server(app);
 var io = require ('socket.io')(server);
+var messages=[{ id:1,
+    texto:"hola soy un mensaje",
+    autor: "Irving Ogaz Uribe"
+}];
 
 //Usamo un middleware para usar elementos estaticos en la seccion publica de la aplicación/
 app.use(express.static('public'));
@@ -14,10 +18,15 @@ app.get('/', function(req,res){
 y tenemos que hacer que el mensaje venga del navegador web mediante html y JS */ 
 io.on('connection', function(socket){
     console.log('Alguien se ha conectado con socket')
-    socket.emit('messages',{
-        id:1,
-        texto:"hola soy un mensaje",
-        autor: "Irving Ogaz Uribe"
+    socket.emit('messages',messages);
+    //ahora queremos escuchar todos los mensajes que envia el cliente
+    socket.on('new-message',function(data){
+        //para  poder guardar estos mensajes lo ideal seria en una base de datos
+        //para este ejercicio utilizaremos arrays (esto no es bueno en produccion)
+
+        messages.push(data);
+        io.sockets.emit('messages',messages);
+
     });
 });
 
